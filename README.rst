6e1b5a60004980f57f56daa5c942c72af685fd73
TsungWei Hu <marr.tw@gmail.com>
2013-08-18 07:52:33 +0800

    Initialize Package.

---
+++ README.rst
@@ -0,0 +1,16 @@
+.. contents::
+
+Introduction
+============
+
+crgis.content offers custom content types for the CRGIS project based on Dexterity.
+
+Installation
+============
+
+Add this line in the eggs section of your ``buildout.cfg``::
+
+    eggs =
+        ...
+        crgis.content
+
---
+++ crgis/content/tests.py
@@ -0,0 +1,58 @@
+import doctest
+import unittest
+
+from Testing import ZopeTestCase as ztc
+
+from Products.Five import zcml
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase.layer import PloneSite
+from Products.PloneTestCase.layer import onsetup
+
+import crgis.content
+
+OPTION_FLAGS = doctest.NORMALIZE_WHITESPACE | \
+               doctest.ELLIPSIS
+
+ptc.setupPloneSite(products=['crgis.content'])
+
+
+class TestCase(ptc.PloneTestCase):
+
+    class layer(PloneSite):
+
+        @classmethod
+        def setUp(cls):
+            zcml.load_config('configure.zcml',
+              crgis.content)
+
+        @classmethod
+        def tearDown(cls):
+            pass
+
+
+def test_suite():
+    return unittest.TestSuite([
+
+        # Unit tests
+        #doctestunit.DocFileSuite(
+        #    'README.txt', package='crgis.content',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+        #doctestunit.DocTestSuite(
+        #    module='crgis.content.mymodule',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+
+        # Integration tests that use PloneTestCase
+        ztc.ZopeDocFileSuite(
+            'INTEGRATION.txt',
+            package='crgis.content',
+            optionflags = OPTION_FLAGS,
+            test_class=TestCase),
+
+        # -*- extra stuff goes here -*-
+
+        ])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
