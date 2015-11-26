d8a0c26e2a0dfc378053c521944acb77de882c68
TsungWei Hu <marr.tw@gmail.com>
2015-11-26 01:46:39 +0000

    Update Setup Script.

--- setup.py
+++ setup.py
@@ -1,44 +1,48 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.3'
+version = '1.0'
 
 setup(name='crgis.content',
-      version=version,
-      description="CRGIS Content Types",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.rst")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+    version=version,
+    description="CRGIS Content Types",
+    long_description=open("README.rst").read() + "\n" +
+                     open(os.path.join("docs", "HISTORY.rst")).read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Environment :: Web Environment",
         "Framework :: Plone",
+        "Framework :: Plone :: 4.3.7",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='Python Plone',
+    author='TsungWei Hu',
+    author_email='marr.tw@gmail.com',
+    url='http://pypi.python.org/pypi/crgis.content',
+    license='GPL version 2',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['crgis'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'plone.api',
+        'setuptools',
+        'plone.app.dexterity',
+        'plone.namedfile [blobs]',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            'plone.app.contenttypes',
+            'plone.app.robotframework [debug]',
         ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['crgis'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.app.dexterity [grok]',
-          'plone.namedfile [blobs]',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      # The next two lines may be deleted after you no longer need
-      # addcontent support from paster and before you distribute
-      # your package.
-#     setup_requires=["PasteScript"],
-#     paster_plugins = ["ZopeSkel"],
-
-      )
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
