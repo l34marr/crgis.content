9e46de32984eda1d763d813e682f35190ae5288b
TsungWei Hu <marr.tw@gmail.com>
2017-07-19 17:55:50 +0800

    Update Interface for Types

--- crgis/content/interfaces.py
+++ crgis/content/interfaces.py
@@ -4,6 +4,6 @@ from zope.interface import Interface
 class IBiXieWu(Interface):
     """BiXieWu Type"""
 
-class ITheater(Interface):
-    """Theater Type"""
+class IPhoto(Interface):
+    """Photo Type"""
 
