ee415b589531f21c460dccd5094214183e8e5e3d
TsungWei Hu <marr.tw@gmail.com>
2018-06-25 22:12:16 +0800

    update 0625-1

--- crgis/content/browser/view.py
+++ crgis/content/browser/view.py
@@ -4,12 +4,60 @@ from zope.schema.interfaces import IVocabularyFactory
 from plone.protect.interfaces import IDisableCSRFProtection
 from zope.interface import alsoProvides
 from plone import api
-import transaction
 
+import pickle
+from collective.geo.geographer.interfaces import IGeoreferenced, IWriteGeoreferenced
+from plone.protect.interfaces import IDisableCSRFProtection
+from zope.interface import alsoProvides
+
+import transaction
 import logging
 
 
 logger = logging.getLogger('SetDefaultViewStatus')
+
+
+#/opt/crgis43f/zinstance/src/crgis.policy/crgis/policy/browser
+
+
+class ImportGIS(BrowserView):
+
+    def __call__(self):
+
+        request = self.request
+        alsoProvides(request, IDisableCSRFProtection)
+
+        with open('/tmp/georesult') as file:
+            result = pickle.load(file)
+
+        total = len(result)
+        count = 0
+        for item in result:
+            if result[item]['type'] == 'Temple':
+                count += 1
+                continue
+
+            brain = api.content.find(
+                Type=result[item]['type'],
+                path=item.replace('Traditional Knowledge and Practices', 'TraditionalKnowledgeAndPractices')
+            )
+            if len(brain) != 1:
+                with open('/tmp/notFound', 'a') as file:
+                    file.write('%s\n' % item)
+                count += 1
+                continue
+#                import pdb; pdb.set_trace()
+
+            obj = brain[0].getObject()
+            geo = IWriteGeoreferenced(obj)
+            geo.setGeoInterface('Point', result[item]['geo'] )
+#            obj.reindexObject(idxs=['zgeo_geometry'])
+
+            # Example, Path: /crgis/temples/ChanghuaCounty/pushin/0715003-FDC, Type: Temple, geo: (Decimal('120.53952200392'), Decimal('23.9525330029991'))
+            count += 1
+            logger.info('%s/%s, Path: %s, Type: %s, geo: %s' % (count, total, item, result[item]['type'], result[item]['geo']))
+
+        return
 
 
 class SetDefaultViewStatus(BrowserView):
---
+++ crgis/content/catalog.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
+from plone.indexer.decorator import indexer
+from BeautifulSoup import BeautifulSoup as bs
+from Products.ATContentTypes.interfaces import IATDocument
+from crgis.content.interfaces import ITemple, IBiXieWu
+
+
+@indexer(IATDocument)
+def bgis_type(obj):
+    if obj.__parent__.getId() == 'bgis':
+        try:
+            k, v = bs(obj.getText()).find('p', {'class': 'ct'}).text.split(': ')
+            if k == u'分類': return v
+        except ValueError:
+            pass
+
+@indexer(IATDocument)
+def area1_bgis(obj):
+    if obj.__parent__.getId() == 'bgis':
+        k, v = bs(obj.getText()).find('p', {'class': 'pr'}).text.split(': ')
+        if k == u'省': return v
+
+@indexer(ITemple)
+def deity_main(obj, **kw):
+    results = []
+    factory = getUtility(IVocabularyFactory, 'vocabulary.deity_name')
+    vocabulary = factory(obj)
+    for value in obj.getDeity_host():
+        try:
+            existing = vocabulary.getTerm(value)
+            results.append(existing.title.split(u': ')[0].encode('utf8'))
+        except LookupError:
+            results.append(value)
+    return results
+
+@indexer(ITemple)
+def deity(obj, **kw):
+    results = []
+    factory = getUtility(IVocabularyFactory, 'vocabulary.deity_name')
+    vocabulary = factory(obj)
+    for value in (set(obj.getDeity_host()) | set(obj.getDeity_company())):
+        try:
+            existing = vocabulary.getTerm(value)
+            results.append(existing.title.split(u': ')[0].encode('utf8'))
+        except LookupError:
+            results.append(value)
+    return results
+
+@indexer(ITemple)
+def wynm(obj):
+    results = []
+    for value in obj.getWysm().split(u';'):
+        if value == u'': continue
+        results.append(u''.join(value.split(u',')))
+    return results
+
+@indexer(ITemple)
+def area1_temple(obj):
+    return obj.__parent__.__parent__.Title()
+
+@indexer(ITemple)
+def area2(obj):
+    return obj.__parent__.__parent__.Title() + obj.__parent__.Title()
+
+@indexer(ITemple)
+def hostcmpn(obj):
+    results = []
+    host = obj.getDeity_host()
+    cmpn = obj.getDeity_company()
+    if 'deity_001' in host or '\xe7\x8e\x8b\xe7\x88\xba' in host:
+        results.append('hwy')
+    if 'deity_001' in cmpn or '\xe7\x8e\x8b\xe7\x88\xba' in cmpn:
+        results.append('cwy')
+    if 'deity_005' in host or '\xe5\xa4\xa9\xe4\xb8\x8a\xe8\x81\x96\xe6\xaf\x8d' in host or '\xe5\xaa\xbd\xe7\xa5\x96' in host:
+        results.append('hmz')
+    if 'deity_005' in cmpn or '\xe5\xa4\xa9\xe4\xb8\x8a\xe8\x81\x96\xe6\xaf\x8d' in cmpn or '\xe5\xaa\xbd\xe7\xa5\x96' in cmpn:
+        results.append('cmz')
+    if 'deity_008' in host or '\xe7\x8e\x84\xe5\xa4\xa9\xe4\xb8\x8a\xe5\xb8\x9d' in host:
+        results.append('hxt')
+    if 'deity_008' in cmpn or '\xe7\x8e\x84\xe5\xa4\xa9\xe4\xb8\x8a\xe5\xb8\x9d' in cmpn:
+        results.append('cxt')
+    return results
+
+@indexer(ITemple)
+def founded(obj):
+    if not obj.era:
+        pass
+    else:
+        try:
+            return int(obj.era)
+        except ValueError:
+            pass
+
+@indexer(IBiXieWu)
+def purpose(obj):
+    return obj.purpose
+
+@indexer(IBiXieWu)
+def material(obj):
+    return obj.material
+
+@indexer(IBiXieWu)
+def locational(obj):
+    return obj.locational
+
+@indexer(IBiXieWu)
+def color(obj):
+    return obj.color
+
+@indexer(IBiXieWu)
+def genre(obj):
+    return obj.genre
+
+@indexer(IBiXieWu)
+def posture(obj):
+    return obj.posture
+
+@indexer(IBiXieWu)
+def gender(obj):
+    return obj.gender
+
+@indexer(IBiXieWu)
+def lct_cou(obj):
+    return obj.lct_cou
+
+@indexer(IBiXieWu)
+def lct_tow(obj):
+    return obj.lct_tow
+
+@indexer(IBiXieWu)
+def lct_vil(obj):
+    return obj.lct_vil
+
+@indexer(IBiXieWu)
+def height(obj):
+    if obj.shi_h:
+        try:
+            value = int(obj.shi_h)
+        except ValueError:
+            value = None
+    else:
+        value = None
+    return value
