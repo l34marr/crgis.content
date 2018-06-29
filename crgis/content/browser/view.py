ba365c900820140e2d6daa224f0edb80b47940a4
TsungWei Hu <marr.tw@gmail.com>
2018-06-29 14:41:38 +0800

    Update Views for Temple and BiXieWu Types

--- crgis/content/browser/view.py
+++ crgis/content/browser/view.py
@@ -1,4 +1,5 @@
 from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from zope.component import getUtility
 from zope.schema.interfaces import IVocabularyFactory
 
@@ -13,4 +14,58 @@ class MyView(BrowserView):
             return term.title
         except:
             return None
+
+class TempleView(BrowserView):
+
+    template = ViewPageTemplateFile("temple.pt")
+
+    def __call__(self):
+        return self.template()
+
+    def deity_term(self, value):
+        factory = getUtility(IVocabularyFactory, 'deity_name')
+        vocabulary = factory(self.context)
+        try:
+            existing = vocabulary.getTerm(value)
+            return existing.title.split(u': ')[0]
+        except LookupError:
+            return value
+
+    def t_title(self, vocab, value):
+        try:
+            factory = getUtility(IVocabularyFactory, vocab)
+            vocabulary = factory(self.context)
+            term = vocabulary.getTerm(value)
+            return term.title
+        except:
+            return None
+
+    def getTemplePilgrimage(self, temple):
+        return None
+
+    def getTempleFestival(self, temple):
+        return None
+
+    def getTempleBiXieWu(self, temple):
+        return None
+
+
+class BiXieWuView(BrowserView):
+
+    template = ViewPageTemplateFile("bixiewu.pt")
+
+    def __call__(self):
+        return self.template()
+
+    def t_title(self, vocab, value):
+        try:
+            factory = getUtility(IVocabularyFactory, vocab)
+            vocabulary = factory(self.context)
+            term = vocabulary.getTerm(value)
+            return term.title
+        except:
+            return None
+
+    def related_temples(self):
+        return None
 
